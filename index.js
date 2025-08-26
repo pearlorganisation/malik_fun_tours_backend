a214b9fbdbb26c325f4ce4562a29c635e995c67d
SakshiSainiPearl <sakshisaini@pearlorganisation.com>
2025-08-26 14:11:26 +0530

    login and signup api

--- index.js
+++ index.js
@@ -1,59 +1,63 @@
-import express from 'express'
-import { configDotenv } from 'dotenv'
-import cookieParser from 'cookie-parser'
-import morgan from 'morgan'
-import cors from 'cors'
-import { errorHandler } from './src/middlewares/errorHandler.js'
-import { connectToMongoDB } from './src/configs/db.js'
-configDotenv()
+import express from "express";
+import { configDotenv } from "dotenv";
+import cookieParser from "cookie-parser";
+import morgan from "morgan";
+import cors from "cors";
+import { errorHandler } from "./src/middlewares/errorHandler.js";
+import { connectToMongoDB } from "./src/configs/db.js";
+import router from "./src/routes/index.js";
+import dotenv from "dotenv";
+dotenv.config();
 
-const app = express()
+// configDotenv();
 
-app.use(cors({
-        origin:
-            process.env.NODE_ENV === "development" ?
-            [
-                "http://localhost:4000",
-                "http://localhost:4002",
-                "http://localhost:4003",
-                "http://localhost:3000",
-                "http://localhost:3001",
-                "http://localhost:3002",
-                "https://page-pop-frontend.vercel.app",
-            ] :
-            [
-                "http://localhost:3000",
-                "http://localhost:3001",
-                "https://page-pop-frontend.vercel.app",
-            ],
-            methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"], // Specify allowed HTTP methods
-            allowedHeaders: ["Content-Type", "Authorization"], // Specify allowed headers
-            credentials: true,
-}))
+const app = express();
+app.use(express.json()); 
+app.use(express.urlencoded({ extended: true })); 
 
-app.use(express.json())
-app.use(express.urlencoded({extended:true}))
-app.use(cookieParser())
-app.use(morgan("dev"))
-app.set("view engine","ejs")
+app.use(
+  cors({
+    origin:
+      process.env.NODE_ENV === "development"
+        ? [
+            "http://localhost:4000",
+            "http://localhost:4002",
+            "http://localhost:4003",
+            "http://localhost:3000",
+            "http://localhost:3001",
+            "http://localhost:3002",
+            "https://page-pop-frontend.vercel.app",
+          ]
+        : [
+            "http://localhost:3000",
+            "http://localhost:3001",
+            "https://page-pop-frontend.vercel.app",
+          ],
+    methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"], // Specify allowed HTTP methods
+    allowedHeaders: ["Content-Type", "Authorization"], // Specify allowed headers
+    credentials: true,
+  })
+);
 
-app.use(`/health`,async(req,res)=>{
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+app.use(morgan("dev"));
+app.set("view engine", "ejs");
+
+app.use(`/health`, async (req, res) => {
   res.status(200).json({ message: "Server is running well", success: true });
-})
+});
 
 /** declare our routes */
 
-
-
-
-
-
+app.use("/api", router);
 
 /** */
 
-app.use(errorHandler)
+app.use(errorHandler);
 
-app.listen(process.env.PORT,async()=>{
-  await connectToMongoDB()
-  console.log(`Database connected and port running on:${process.env.PORT}`)
-})
+app.listen(process.env.PORT, async () => {
+  await connectToMongoDB();
+  console.log(`Database connected and port running on:${process.env.PORT}`);
+});
---
+++ src/routes/user.js
@@ -0,0 +1,9 @@
+import express from "express";
+import * as userController from "../controllers/userController.js";
+
+const router = express.Router();
+
+router.post(`/signup`, userController.signup);
+router.post(`/login`, userController.login);
+
+export default router;
