3861f3b7d0290e454a4afdb4ca6ed1553f9c4e01
kartikperal <kartikchauhan@pearlorganisation.com>
2026-01-02 17:18:18 +0530

    updated

--- constants.js
+++ constants.js
@@ -2,7 +2,7 @@ export const DB_NAME = "fun_tours";
 
 export const COOKIE_OPTIONS = {
   httpOnly: true,
-  sameSite: process.env.NODE_ENV === "production" ? "None" : "Lax", // allow OAuth redirect
+  sameSite: "None", // allow OAuth redirect
   secure: process.env.NODE_ENV === "production",
 };
 
