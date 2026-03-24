876fe43629ee9d9f5faf589d963a2333b176b55e
Pearl Organisation™ <cloudpearlorganisation@gmail.com>
2026-03-24 14:03:27 +0530

    added

--- src/routes/order.routes.js
+++ src/routes/order.routes.js
@@ -1,12 +1,13 @@
 import express from "express"
 import { createOrder } from "../controllers/order.controller.js"
+import { authenticateToken } from "../middlewares/authMiddleware.js";
 
 
 
 const orderRoute = express.Router()
 
 
-orderRoute.post("/create" , createOrder)
+orderRoute.post("/create" , authenticateToken , createOrder)
 
 
 export default orderRoute;
